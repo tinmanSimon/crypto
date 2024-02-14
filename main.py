67695996fed044312532e7171e1bc88588dea2be
Simon <wudisas@gmail.com>
2024-02-14 15:28:05 -0800

    beautified json result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cryptoData import cryptoData
 from mongoDBUtils import mongoProject
-import flask
+import flask 
+from flask import render_template
+import json
 
 # cryptoDataHandler = cryptoData(True)
 # data = cryptoDataHandler.getTopVolatileProducts()
@@ -14,5 +16,6 @@ def get_mongo_records(request: flask.Request) -> flask.Response:
     print("get_mongo_records started")
     cryptoProject = mongoProject()
     cryptoProject.setCollection("crypto_analytics", "test_collection")
-    df = cryptoProject.getDataframe()
-    return df.to_dict('records')
+    dataFrame = cryptoProject.getDataframe()
+    jsonResult = json.dumps(dataFrame.to_dict('records'), indent=4)
+    return render_template('index.html', data=jsonResult)
