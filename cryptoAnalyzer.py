e156e8e29c1a9942163ffcbc13d30e399ac2217a
Simon <wudisas@gmail.com>
2024-04-13 15:39:06 -0400

    annotation shows up for bars, but not quite right

--- cryptoAnalyzer.py
+++ cryptoAnalyzer.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 def getTradeProfits():
     tradingHistory = pd.read_csv("./logs/paper-trading-history-all-2024-04-13T15_59_37.071Z.csv")
@@ -26,10 +27,19 @@ def getTradeProfits():
                 })
                 accumulatedProfits = 0
                 print("\n")
-    profitHistory = pd.DataFrame(newRows, columns=['Symbol', 'Profit']).sort_values(by='Profit')
+    if not newRows: 
+        print("Didn't found any completed trades, return")
+        return
+    profitHistory = pd.DataFrame(newRows, columns=newRows[0].keys()).sort_values(by='Profit')
     print(profitHistory)
     print(profitHistory['Profit'].sum())
-    profitHistory.plot.bar(x='Symbol', y='Profit')
+    ax = profitHistory.plot.bar(use_index=True, y='Profit')
+    for p in ax.patches:
+        x_pos = p.get_x() + p.get_width() / 2
+        row = profitHistory.iloc[int(x_pos)]
+        tmp = "\n".join([str(val) for val in row['ads']])
+        ax.annotate(tmp, (p.get_x() * 1.005, p.get_height() * 1.005))
+
 
 getTradeProfits()
 
